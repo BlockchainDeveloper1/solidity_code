d7a59c9bbbc7973e355ca0f9cd5a5c7b917867eb
Sanjana <sanjanagoutam062@gmail.com>
2025-01-09 19:21:54 +0530

    Complete remaining code

--- Solidity/ArryandStruct.sol
+++ Solidity/ArryandStruct.sol
@@ -2,11 +2,24 @@
 
 pragma solidity ^0.8.28 ;
 
-contract structandarrytogether{
+contract StructInArry{
     
    struct Person{
     string Name ;
-    uint Number;
+    address Address;
+  }
+
+  Person[] public person;
+
+  //function to add value in struct arry
+
+  function add(string memory _Name , address _Address)public{
+    person.push(Person(_Name , _Address));
+  } 
+
+  function retrievestruct(uint _i)public view returns(string memory , address){
+    Person memory p = person[_i]; //Access value of person on _i
+    return (p.Name , p.Address);  //return value from struct
   }
 
 }
