b9e7de720883153d495e0e0f5856de44686e4406
Sanjana <sanjanagoutam062@gmail.com>
2025-03-31 20:57:57 +0530

    concept of solidity

--- Solidity/contracts/SimpleStorage2.sol
+++ Solidity/contracts/SimpleStorage2.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.29;
 
 contract SimpleStorage{
     /*
-    //Array : Simply array is set of elements of same data typ
+    //Array : Simply array is set of elements of same data type.
     
 
     */
