0c471c817c617d126c0b5321583b958c5f69f52a
Sanjana <sanjanagoutam062@gmail.com>
2025-04-11 20:27:23 +0530

    concept of solidity

--- Solidity/contracts/SimpleStorage2.sol
+++ Solidity/contracts/SimpleStorage2.sol
@@ -6,7 +6,7 @@ contract SimpleStorage{
     /*
     //Array : Simply array is set of elements of same data type.
 
-    string[] public names = ["joy" , "jenni" , "peter" , "hero" ] ; //This is a  
+    string[] public names = ["joy" , "jenni" , "peter" , "hero" ] ; //This is an  
     
 
     */
