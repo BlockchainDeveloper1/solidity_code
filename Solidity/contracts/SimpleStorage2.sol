81a2cd8af1da99a3a0dc56694763656acea69352
Sanjana <sanjanagoutam062@gmail.com>
2025-04-17 23:08:16 +0530

    concept of solidity

--- Solidity/contracts/SimpleStorage2.sol
+++ Solidity/contracts/SimpleStorage2.sol
@@ -8,7 +8,7 @@ contract SimpleStorage{
 
     string[] public names = ["joy" , "jenni" , "peter" , "hero" ] ; //This is an example of dynamic array.
 
-    string[4] private pe 
+    string[4] private pet 
     
 
     */
